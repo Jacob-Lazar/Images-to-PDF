49db4b65693fb8b61cb98fc64db8eb0ab68ce142
Jacob Lazar <129856302+Jacob-Lazar@users.noreply.github.com>
2023-12-09 09:44:59 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import streamlit as st
 from PIL import Image
 import img2pdf
+import io
 
 # Title of the app
-st.title('Images to PDF converter')
+st.title('Image to PDF converter')
 
 # Upload images
 uploaded_files = st.file_uploader("Choose Images", accept_multiple_files=True)
 
 if st.button('Convert to PDF'):
     if uploaded_files:
-        images = [Image.open(uploaded_file) for uploaded_file in uploaded_files]
-
         # Convert images to PDF
-        pdf_bytes = img2pdf.convert([image.filename for image in images])
+        pdf_bytes = img2pdf.convert([uploaded_file.read() for uploaded_file in uploaded_files])
 
         # Create a link to download the PDF
         st.download_button(
             label="Download PDF",
             data=pdf_bytes,
-            file_name='Image_to_PDF.pdf',
+            file_name='output.pdf',
             mime='application/pdf',
         )
     else:
