e0ac49aa1ffb8466da5af939e5aa8aeb9ed2da40
Jacob Lazar <129856302+Jacob-Lazar@users.noreply.github.com>
2023-12-09 05:01:55 +0000

    update

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ if st.button('Convert to PDF'):
     if uploaded_files:
         # Convert images to RGB mode
         images = [Image.open(uploaded_file).convert('RGB') for uploaded_file in uploaded_files]
-
+        
         # Convert images to PDF
-        pdf_bytes = img2pdf.convert([image.tobytes() for image in images])
-
+        pdf_bytes = img2pdf.convert([image for image in images])
+        
         # Create a link to download the PDF
         st.download_button(
             label="Download PDF",
